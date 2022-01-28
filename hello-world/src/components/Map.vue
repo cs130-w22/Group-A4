917eda81fb75b4f7bc9b55092bd7174b47e31951
dyyfk <879608095@qq.com>
2022-01-28 15:27:07 -0800

    get users coordinates from browser

---
+++ hello-world/src/components/Map.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Your coordinates:</h1>
+        <p>{{ coordinates.lat }} Latitude, {{coordinates.lng }} Longtitude </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            coordinates: {
+                lat : 0,
+                lng : 0
+            }
+        }
+    },
+
+    created(){
+        this.$getLocation({})
+            .then(coordinates=>{
+                this.coordinates = coordinates;
+            });
+    }
+}
+</script>
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGeolocation from 'vue-browser-geolocation'
 
 Vue.config.productionTip = false
+Vue.use(VueGeolocation)
 
 new Vue({
   render: h => h(App),
