23476cd34a5aa393a051cd9f56b7842207d990af
dyyfk <879608095@qq.com>
2022-01-28 16:06:24 -0800

    import google map to the page

--- hello-world/src/components/Map.vue
+++ hello-world/src/components/Map.vue
@@ -2,11 +2,20 @@
     <div>
         <h1>Your coordinates:</h1>
         <p>{{ coordinates.lat }} Latitude, {{coordinates.lng }} Longtitude </p>
+        <gmap-map
+        :center="{lat:10, lng:10}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+        >
+        </gmap-map>>
+            
     </div>
 </template>
 
 <script>
 export default {
+    name: 'GoogleMap',
     data() {
         return {
             coordinates: {
@@ -20,7 +29,7 @@ export default {
         this.$getLocation({})
             .then(coordinates=>{
                 this.coordinates = coordinates;
-            });
+            }).catch(error => alert(error));
     }
 }
 </script>
--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -5,6 +5,13 @@ import VueGeolocation from 'vue-browser-geolocation'
 Vue.config.productionTip = false
 Vue.use(VueGeolocation)
 
+import * as VueGoogleMaps from 'vue2-google-maps'
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: ''
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
